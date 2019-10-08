cf37188b0ac6c99b919af5a3640f38c5737ae3ca
Rohit Ramesh <rkr@berkeley.edu>
2019-10-07 19:20:50 -0700

    Notes on future work

--- src/Example.hs
+++ src/Example.hs
@@ -18,3 +18,15 @@ setup window = do
     liftIO $ modifyIORef count (+ 1)
     n <- liftIO $ readIORef count
     element button # set UI.text ("I have been clicked " <> show n <> " times!")
+
+{-
+  Things to try
+  - Way to generate electron.js
+  - Atom style panes and stuff?
+  - Tabs and multiple windows
+  - do more of the setup and stuff in Setup.hs
+  - multi-process versions of gui things
+  - better understand the FRP model of threepenny-gui
+
+  - Okay, the comm model is basically just "run this JS to do the thing" ...
+-}
